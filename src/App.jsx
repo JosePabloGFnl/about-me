7bc5bb8d889ca70a5773aa0bbcfd295032246349
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2023-10-03 13:48:24 +0000

    Achievements section added

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import Footer from "./Components/Footer";
 import Header from "./Components/Header";
 import Home from "./Components/Home";
 import Portfolio from "./Components/Portfolio";
+import Achievements from "./Components/Achievements";
 
 import "./styles.css";
 
@@ -45,6 +46,7 @@ const App = () => {
       <Home name={siteProps.name} title={siteProps.title} />
       <About />
       <Portfolio />
+      <Achievements />
       <Footer {...siteProps} primaryColor={primaryColor} secondaryColor={secondaryColor} />
     </div>
   );
