3b9c9f86c4f09a0cc476d9551b6def120031a0e3
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2023-10-03 12:12:27 +0000

    Projet list updated

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "humpback whale breaching";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Kaban",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Centralization tool for research material, consulting it with the power of AI and LLMs.",
+    url: "https://github.com/Special-Unitary-Group/kaban",
   },
   {
-    title: "Web Development for Beginners",
+    title: "TigerEye",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Analysis and visualization tool for correlations, tendencies and other factors in cities.",
+    url: "https://github.com/Special-Unitary-Group/tigereye",
   },
   {
-    title: "My Resume Site",
+    title: "Prize-Collecting Travelling Salesman Problem",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "Solutions on the prize collecting travelling salesman problem with two constructive heuristics and a local search method.",
+    url: "https://github.com/JosePabloGFnl/Prize-Collecting-Travelling-Salesman-Problem",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "San Nicolas de los Garza - COVID Cases vs. Complaints per Affected Legal Good",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Investigation of the correlation between monthly COVID-19 cases in San Nicolás de los Garza, Nuevo León, México and complaints according to affected legal goods in the city.",
+    url: "https://github.com/JosePabloGFnl/San-Nico-COVID-Cases-Vs-Complaints",
   },
 ];
 
