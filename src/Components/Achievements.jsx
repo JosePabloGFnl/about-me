3a42dc69cb122e5fed618d00ef4c288e749ee7c9
Pablo González <71510885+JosePabloGFnl@users.noreply.github.com>
2023-10-03 13:50:43 +0000

    New files added

---
+++ src/Components/Achievements.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Achievements = () => {
+    return(
+        <section className="light" id="achievements">
+            <h2>Achievements</h2>
+        </section>
+    )
+}
+
+export default Achievements;
